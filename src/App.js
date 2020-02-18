56a156fe5c95e233f90c367a90632a27db3fca52
Felix-Suen <felix.suen2000@gmail.com>
2020-02-17 17:00:02 -0800

    updated

--- src/App.js
+++ src/App.js
@@ -3,60 +3,66 @@ import './App.css';
 import Mouse from './components/mouse';
 
 class App extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      daynum: this.Daynum() + ' Days',
+    constructor(props) {
+        super(props);
+        this.state = {
+            daynum: this.Daynum(),
+        }
     }
-  }
 
-  Daynum() {
-    var currentDate = new Date();
-    var firstDate = new Date("09/21/2018");
-    var time_difference = currentDate.getTime() - firstDate.getTime();
-    var day_difference = Math.floor(time_difference / (1000 * 3600 * 24));
-    return day_difference;
-  }
+    Daynum() {
+        var currentDate = new Date();
+        var firstDate = new Date("09/21/2018");
+        var time_difference = currentDate.getTime() - firstDate.getTime();
+        var day_difference = Math.floor(time_difference / (1000 * 3600 * 24));
+        return day_difference;
+    }
 
-  current() {
-    var currentDate = new Date();
-    return currentDate;
-  }
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                daynum: this.Daynum()
+            })
+        }, 360000)
+    }
 
-  componentDidMount() {
-    setInterval( () => {
-      this.setState({
-        daynum : this.Daynum()
-      })
-    },360000)
-  }
+    render() {
+        return (
+            <div className="background">
 
-  render() {
-    return(
-      <div className="background">
+                <Mouse days={this.state.daynum} />
 
-      <Mouse days={this.state.daynum}/>
+                <div className="row">
 
-        <div className="row">
-          <div className="column">
-            <a href="https://www.google.com"><div className="invibutton"></div></a>
-          </div>
-          <div className="column">
-            <a href="https://www.youtube.com"><div className="invibutton"></div></a>
-          </div>
-        </div>
+                    <div className="column">
+                        <a href="https://owl.uwo.ca/portal">
+                            <div className="invibutton"></div>
+                        </a>
+                    </div>
 
-        <div className="row">
-          <div className="column">
-            <a href="https://www.messenger.com"><div className="invibutton"></div></a>
-          </div>
-          <div className="column">
-            <a href="https://www.gmail.com"><div className="invibutton"></div></a>
-          </div>
-        </div>
-      </div>
-    );
-  }
+                    <div className="column">
+                        <a href="https://outlook.office.com/mail/inbox">
+                            <div className="invibutton"></div>
+                        </a>
+                    </div>
+                </div>
+
+                <div className="row">
+
+                    <div className="column">
+                        <a href="https://drive.google.com/drive/u/0/my-drive">
+                            <div className="invibutton"></div>
+                        </a>
+                    </div>
+                    <div className="column">
+                        <a href="https://mail.google.com/mail/ca/u/0/#inbox">
+                            <div className="invibutton"></div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
